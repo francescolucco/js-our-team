2e6a288b7cf50ead7d48a5e2d8359f1fa2650b66
francescolucco <francescolucco@googlemail.com>
2021-11-04 17:12:57 +0100

    stampa dati su HTML

--- js/script.js
+++ js/script.js
@@ -1,31 +1,62 @@
 // 1. Creo un array contente gli oggetti e le relative proprietà e valori
-const teamCard = {
-    image: ['wayne-barnett-founder-ceo.jpg','angela-caroll-chief-editor.jpg','angela-lopez-social-media-manager.jpg',
-    'barbara-ramos-graphic-designer.jpg',
-    'scott-estrada-developer.jpg',
-    'walter-gordon-office-manager.jpg',
-    'walter-gordon-office-manager.jpg'],
-    firstname: ['Wayne','Angela', 'Angela', 'Barbara', 'Scott', 'Walter'],
-    lastname: ['Barnett', 'Caroll', 'Lopez', 'Ramos', 'Estrada', 'Gordon'],
-    role: ['Founder & CEO', 'Chief Editor', 'Social Media Manager', 'Graphic Designer', 'Developer', 'Office Manager'],
-  }
-console.log(teamCard);
+const teamCard = [
+  {
+    cardImage: 'wayne-barnett-founder-ceo.jpg',
+    name: 'Wayne Barnet',
+    role: 'Founder & CEO',
+  },
+  {
+    cardImage: 'angela-caroll-chief-editor.jpg',
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+  },
+  {
+    cardImage: 'angela-lopez-social-media-manager.jpg',
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+  },
+  {
+    cardImage: 'barbara-ramos-graphic-designer.jpg',
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+  },
+  {
+    cardImage: 'scott-estrada-developer.jpg',
+    name: 'Scott Estrada',
+    role: 'Developer',
+  },
+  {
+    cardImage: 'walter-gordon-office-manager.jpg',
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+  },
+
+]
+
+
 
 // creo una variabile vuota contente una stringa che utilizzerò per stamapare il testo HTML
 let html = '';
 // creo la costante che mi aggancia il punto su cui andare a stampare sul DOM
-const teamContainer = document.querySelector('team-container');
+const teamContainer = document.querySelector('.team-container');
 
 // 3. Utilizzo un ciclo "for in" per ricavare tutte le informazioni dei singoli utenti
-for(let i = 0; i < teamCard.image.length; i++){
-    html = 
+for(let i = 0; i < teamCard.length; i++){
+
+    // let htmlDeiRuoli = '';
+
+    // for(let indiceRole = 0; indiceRole < teamCard.role) {
+    //   htmlDeiRuoli += `<p>${teamCard.role[indiceRole]</p>`
+    // }
+
+    html += 
     `<div class="team-card">
       <div class="card-image">
-        <img src="{${teamCard.image[i]}}" alt="${teamCard.firstname[i]} ${teamCard.lastname[i]}" />
+        <img src="img/${teamCard[i].cardImage}" alt="${teamCard[i].name}" />
       </div>
       <div class="card-text">
-        <h3>${teamCard.firstname[i]} ${teamCard.lastname[i]}</h3>
-        <p>${teamCard.role[i]}</p>
+        <h3>${teamCard[i].name}</h3>
+        <p>${teamCard[i].role}</p>
       </div>
     </div>`
     console.log(html);
@@ -36,47 +67,3 @@ for(let i = 0; i < teamCard.image.length; i++){
 
 
 
-// const teamCard = [
-//   {
-//     cardImage: 'wayne-barnett-founder-ceo.jpg',
-//     firstname: 'Wayne',
-//     lastname: 'Barnett',
-//     role: 'Founder & CEO',
-//   },
-//   {
-//     cardImage: 'angela-caroll-chief-editor.jpg',
-//     firstname: 'Angela',
-//     lastname: 'Caroll',
-//     role: 'Chief Editor',
-//   },
-//   {
-//     cardImage: 'angela-lopez-social-media-manager.jpg',
-//     firstname: 'Angela',
-//     lastname: 'Lopez',
-//     role: 'Social Media Manager',
-//   },
-//   {
-//     cardImage: 'barbara-ramos-graphic-designer.jpg',
-//     firstname: 'Barbara',
-//     lastname: 'Ramos',
-//     role: 'Graphic Designer',
-//   },
-//   {
-//     cardImage: 'scott-estrada-developer.jpg',
-//     firstname: 'Scott',
-//     lastname: 'Estrada',
-//     role: 'Developer',
-//   },
-//   {
-//     cardImage: 'walter-gordon-office-manager.jpg',
-//     firstname: 'Walter',
-//     lastname: 'Gordon',
-//     role: 'Office Manager',
-//   },
-//   {
-//     cardImage: 'walter-gordon-office-manager.jpg',
-//     firstname: 'Walter',
-//     lastname: 'Gordon',
-//     role: 'Office Manager',
-//   },
-// ]
