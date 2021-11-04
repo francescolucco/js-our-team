eceedde2ffa84b55b1bf8357d8bc0cd7b50de2b1
francescolucco <francescolucco@googlemail.com>
2021-11-04 15:37:45 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const teamCard = [
+  {
+    cardImage: 'wayne-barnett-founder-ceo.jpg',
+    firstname: 'Wayne',
+    lastname: 'Barnett',
+    role: 'Founder & CEO',
+  },
+  {
+    cardImage: 'angela-caroll-chief-editor.jpg',
+    firstname: 'Angela',
+    lastname: 'Caroll',
+    role: 'Chief Editor',
+  },
+  {
+    cardImage: 'angela-lopez-social-media-manager.jpg',
+    firstname: 'Angela',
+    lastname: 'Lopez',
+    role: 'Social Media Manager',
+  },
+  {
+    cardImage: 'barbara-ramos-graphic-designer.jpg',
+    firstname: 'Barbara',
+    lastname: 'Ramos',
+    role: 'Graphic Designer',
+  },
+  {
+    cardImage: 'scott-estrada-developer.jpg',
+    firstname: 'Scott',
+    lastname: 'Estrada',
+    role: 'Developer',
+  },
+  {
+    cardImage: 'walter-gordon-office-manager.jpg',
+    firstname: 'Walter',
+    lastname: 'Gordon',
+    role: 'Office Manager',
+  },
+  {
+    cardImage: 'walter-gordon-office-manager.jpg',
+    firstname: 'Walter',
+    lastname: 'Gordon',
+    role: 'Office Manager',
+  },
+]
